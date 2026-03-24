bd38770d282df044a88a24c126ef5b139bd60c46
Guilherme Firmino da Silva. <guilhermefsilva.09012009@gmail.com>
2026-03-24 15:22:34 -0300

    update-primeiro modulo

---
+++ modulo_1/README.md
@@ -0,0 +1,5 @@
+Sobre o codigo:
+
+fiz uma simples entrada com a pergunta-> "você esta bem" e com varios tipos de respostas e ate alguns easter eggs engraçados.
+
+espero que goste!
--- modulo_1/introdução_git_github_e_metodologias_ágeis.py
+++ modulo_1/introdução_git_github_e_metodologias_ágeis.py
@@ -5,3 +5,33 @@ CRUD
 apresentacao do git e github
 
 '''
+
+print("bom dia tudo bem? (^_^) ")
+if input("como você está?") == "estou bem":
+    print("que bom :-D")
+
+elif input("como você está?") == "estou mal":
+    print("espero que fique bem logo :-) ")
+
+elif input("como você está?") == "estou cansado":
+    print("espero que descanse e fique bem logo ;-) ")
+
+elif input("como você está?") == "estou bem, e vc?":
+    print("estou bem,que bom :-D e obrigado por perguntar")
+
+elif input("como você está?") == "estou mal, e vc?":
+    print("estou bem,espero que fique bem logo :-) e obrigado por perguntar")
+
+elif input("como você está?") == "estou cansado, e vc?":
+    print("estou bem,espero que descanse e fique bem logo ;-) e obrigado por perguntar")
+
+elif input("como você está?") == "asdghjkln":
+    print("e... ok entao, espero que tenha um bom dia... eu acho (●__●)")
+
+elif input("como você está?") == "modo autodestruição":
+    print("perai, o que? w(ﾟДﾟ)w naaaãaãããããaaaaaõooooooooo (barulho de explosão)")
+
+elif input("como você está?") == ("não é da sua conta"):
+    print("ta bom né seu palhaço, iguinorancia é uma coisa feia, espero que sua vida seja melhor do que sua resposta, e que você tenha um bom dia (¬_¬ )")
+
+else:  print("nao entendi, mas espero que tenha um bom dia! =-) ")
