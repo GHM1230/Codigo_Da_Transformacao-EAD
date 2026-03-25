37d986307f2d4f50df722325f5535fc5ccfb0859
Guilherme firmino <guilhermefsilva.09012009@gmail.com>
2026-03-25 15:56:37 -0300

    update-modulo 1 atualizado

--- modulo_1/introdução_git_github_e_metodologias_ágeis.py
+++ modulo_1/introdução_git_github_e_metodologias_ágeis.py
@@ -7,31 +7,33 @@ apresentacao do git e github
 '''
 
 print("bom dia tudo bem? (^_^) ")
-if input("como você está?") == "estou bem":
+resposta = input("como você está?")
+
+if resposta == "estou bem":
     print("que bom :-D")
 
-elif input("como você está?") == "estou mal":
+elif resposta == "estou mal":
     print("espero que fique bem logo :-) ")
 
-elif input("como você está?") == "estou cansado":
+elif resposta == "estou cansado":
     print("espero que descanse e fique bem logo ;-) ")
 
-elif input("como você está?") == "estou bem, e vc?":
+elif resposta == "estou bem, e vc?":
     print("estou bem,que bom :-D e obrigado por perguntar")
 
-elif input("como você está?") == "estou mal, e vc?":
+elif resposta == "estou mal, e vc?":
     print("estou bem,espero que fique bem logo :-) e obrigado por perguntar")
 
-elif input("como você está?") == "estou cansado, e vc?":
+elif resposta == "estou cansado, e vc?":
     print("estou bem,espero que descanse e fique bem logo ;-) e obrigado por perguntar")
 
-elif input("como você está?") == "asdghjkln":
+elif resposta == "asdghjkln":
     print("e... ok entao, espero que tenha um bom dia... eu acho (●__●)")
 
-elif input("como você está?") == "modo autodestruição":
+elif resposta == "modo autodestruição":
     print("perai, o que? w(ﾟДﾟ)w naaaãaãããããaaaaaõooooooooo (barulho de explosão)")
 
-elif input("como você está?") == ("não é da sua conta"):
-    print("ta bom né seu palhaço, iguinorancia é uma coisa feia, espero que sua vida seja melhor do que sua resposta, e que você tenha um bom dia (¬_¬ )")
+elif resposta == ("não é da sua conta"):
+    print("ta bom né seu palhaço, ignorancia é uma coisa feia, espero que sua vida seja melhor do que sua resposta, e que você tenha um bom dia (¬_¬ )")
 
 else:  print("nao entendi, mas espero que tenha um bom dia! =-) ")
